628298dc921dc4fe0a414ce2f717b5c539f1e6e4
Darshi Shah <darshi24.shah@gmail.com>
2022-02-21 14:40:54 -0500

    updated connection string in server.ts

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ import MessageController from "./controllers/MessageController";
 
 const app = express();
 // mongoose.connect('mongodb://localhost:27017/tuits-trial2');
-mongoose.connect('mongodb+srv://darshi24:'+process.env.TUITER_PASSWORD+'@tuitercluster.cr6mb.mongodb.net/test');
+mongoose.connect('mongodb+srv://darshi24:'+process.env.TUITER_PASSWORD+'@cluster0.vhlkj.mongodb.net/test');
 app.use(bodyParser.json())
 
 app.get('/hello', (req, res) =>
